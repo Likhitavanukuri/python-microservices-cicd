23fad6ab47568fbc9b038f82a93e7c16ecb68835
Likhitavanukuri <vanukurilikhita@gmail.com>
2025-12-05 10:27:41 +0000

    Dynamic port logic based on service name

--- common-shared-lib/vars/deployApp.groovy
+++ common-shared-lib/vars/deployApp.groovy
@@ -1,6 +1,12 @@
 def call(String imageName) {
     echo "🚀 Deploying container using image: ${imageName}"
 
+    // Determine port based on service name
+    def port = "4001"  // default for greeting-service
+    if (env.JOB_NAME == "time-service") {
+        port = "4002"
+    }
+
     sh """
         echo Stopping old container if exists...
         docker stop ${env.JOB_NAME} || true
@@ -8,7 +14,7 @@ def call(String imageName) {
         echo Removing old container...
         docker rm ${env.JOB_NAME} || true
 
-        echo Running new container...
-        docker run -d --name ${env.JOB_NAME} -p 4002:4002 ${imageName}
+        echo Running new container on port ${port}...
+        docker run -d --name ${env.JOB_NAME} -p ${port}:${port} ${imageName}
     """
 }

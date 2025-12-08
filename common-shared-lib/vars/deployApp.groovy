0851016a8881efb71a5d15bb3e4d9e99696113e3
Likhitavanukuri <vanukurilikhita@gmail.com>
2025-12-08 09:21:46 +0000

    Added health check to deployApp.groovy

--- common-shared-lib/vars/deployApp.groovy
+++ common-shared-lib/vars/deployApp.groovy
@@ -1,8 +1,9 @@
 def call(String imageName) {
     echo "🚀 Deploying container using image: ${imageName}"
     echo "🔥 Shared Library Updated Successfully!"
+    echo "⏱ Deployment triggered at: ${new Date()}"
 
-    // Determine port based on service name
+    // Decide port based on microservice name
     def port = "4001"  // default for greeting-service
     if (env.JOB_NAME == "time-service") {
         port = "4002"
@@ -17,5 +18,9 @@ def call(String imageName) {
 
         echo Running new container on port ${port}...
         podman run -d --name ${env.JOB_NAME} -p ${port}:${port} ${imageName}
+
+        echo '🔍 Performing health check...'
+        sleep 3
+        curl -s http://localhost:${port} || echo '⚠️ Service did not respond'
     """
 }
